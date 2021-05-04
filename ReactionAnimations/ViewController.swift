519ec84cc83bc2e67d3c46073c1ca76b3fe49b39
Nishant Taneja <nishant.taneja2701@gmail.com>
2021-05-05 02:24:11 +0530

    add Icon Animations

--- ReactionAnimations/ViewController.swift
+++ ReactionAnimations/ViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet private weak var button: UIButton!
     
-    private let iconsContainerView: UIView = {
+    private let iconHeight: CGFloat = 48
+    private let padding: CGFloat = 8
+    
+    private lazy var iconsContainerView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
-        let iconHeight: CGFloat = 48
         view.layer.cornerRadius = iconHeight / 2
-        let padding: CGFloat = 8
-        let arrangedSubViews = [#imageLiteral(resourceName: "blue_like"), #imageLiteral(resourceName: "red_heart"), #imageLiteral(resourceName: "surprised"), #imageLiteral(resourceName: "cry_laugh"), #imageLiteral(resourceName: "cry"), #imageLiteral(resourceName: "angry")].compactMap { image -> UIView in
+        view.layer.shadowColor = UIColor.darkGray.cgColor
+        view.layer.shadowRadius = iconHeight/2
+        view.layer.shadowOpacity = 0.5
+        view.layer.shadowOffset = .zero
+        // StackView
+        let arrangedSubViews = [#imageLiteral(resourceName: "blue_like"), #imageLiteral(resourceName: "red_heart"), #imageLiteral(resourceName: "surprised"), #imageLiteral(resourceName: "cry_laugh"), #imageLiteral(resourceName: "cry"), #imageLiteral(resourceName: "angry")].compactMap { image -> UIImageView in
             let imageView = UIImageView(image: image)
             imageView.layer.cornerRadius = iconHeight/2
+            imageView.isUserInteractionEnabled = true
             return imageView
         }
         let stackView = UIStackView(arrangedSubviews: arrangedSubViews)
@@ -29,10 +36,6 @@ class ViewController: UIViewController {
         view.addSubview(stackView)
         let numberOfIcons = CGFloat(arrangedSubViews.count)
         stackView.frame.size = CGSize(width:  numberOfIcons * iconHeight + (numberOfIcons + 1)*padding, height: iconHeight + (2 * padding))
-        view.layer.shadowColor = UIColor.darkGray.cgColor
-        view.layer.shadowRadius = iconHeight/2
-        view.layer.shadowOpacity = 0.5
-        view.layer.shadowOffset = .zero
         view.frame.size = stackView.frame.size
         return view
     }()
@@ -70,16 +73,34 @@ extension ViewController {
     }
     
     private func handleStateChanged(_ gesture: UILongPressGestureRecognizer) {
-        print(#function)
+        let location = gesture.location(in: iconsContainerView)
+        let point = CGPoint(x: location.x, y: iconsContainerView.frame.height/2)
+        guard let icon = iconsContainerView.hitTest(point, with: nil) as? UIImageView else {
+            return
+        }
+        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: .curveEaseOut) {
+            self.pullDownAllIcons()
+            icon.transform = .init(translationX: 0, y: -50)
+        }
     }
     
     private func handleStateEnded(_ gesture: UILongPressGestureRecognizer) {
         UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: .curveEaseIn) {
+            self.pullDownAllIcons()
             self.iconsContainerView.transform = .identity
             self.iconsContainerView.alpha = 0
         } completion: { _ in
             self.iconsContainerView.removeFromSuperview()
         }
+    }
+}
 
+extension ViewController {
+    private func pullDownAllIcons() {
+        if let stackView = self.iconsContainerView.subviews.first as? UIStackView {
+            stackView.subviews.forEach { view in
+                view.transform = .identity
+            }
+        }
     }
 }
